710656ff5a4dfa36b97d9119720dea2a3600c319
Danton Matheus <dantoncosta03@hotmail.com>
2022-09-21 01:10:20 -0300

    :sparkles: feat: Testes automatizados para a rota de login

--- tests/authTests.test.ts
+++ tests/authTests.test.ts
@@ -7,7 +7,7 @@ beforeEach(async () => {
   await prisma.$executeRaw`TRUNCATE users RESTART IDENTITY`;
 });
 
-describe('Testa a função de cadastro POST /signup', () => {
+describe('Testa a rota de cadastro POST /signup', () => {
   it('Deve cadastrar o usuário com sucesso', async () => {
     const user = await userFactory();
     const result =  await supertest(app).post('/signup').send(user);
@@ -19,6 +19,25 @@ describe('Testa a função de cadastro POST /signup', () => {
     await supertest(app).post("/signup").send(user);
     const result = await supertest(app).post("/signup").send(user);
     expect(result.status).toEqual(409);
+  });
+});
+
+describe("Testa a rota de login POST /login", () => {
+  it("Deve receber um retornar status code 200 e um token", async () => {
+    const user = await userFactory();
+    await supertest(app).post("/signup").send(user);
+    delete user.confirmPassword;
+    const result = await supertest(app).post("/login").send(user);
+    expect(result.status).toEqual(200);
+    expect(result.body).toBeInstanceOf(Object);
+    expect(result.body.token).not.toBeUndefined();
+  });
+
+  it("Deve retornar 401 ao enviar um usuário com credenciais inválidas", async () => {
+    const randomUser = await userFactory();
+    delete randomUser.confirmPassword;
+    const result = await supertest(app).post("/login").send(randomUser);
+    expect(result.status).toEqual(401);
   });
 });
 
